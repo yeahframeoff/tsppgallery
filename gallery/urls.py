02bb1285e11b4aaad8cb4e0d29c3bbe9866791bf
Anton Yefremov <anthyef@gmail.com>
2015-04-13 23:02:01 +0300

    added registration

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,4 +7,5 @@ urlpatterns = (
     url(r'^$', views.main, name='mainpage'),
     url(r'^login/?$', authviews.login, {'template_name': 'templates/login.html', 'current_app': 'gallery'}, name='login'),
     url(r'^logout/?$', authviews.logout, {'next_page': 'mainpage', 'current_app': 'gallery'}, name='logout'),
+    url(r'^register/?$', views.register, name='register'),
 )
--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.forms import Form, fields
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
 
 def main(request):
     return render(request, 'mainpage.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            new_user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+            login(request, new_user)
+            return HttpResponseRedirect(reverse('mainpage'))
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
