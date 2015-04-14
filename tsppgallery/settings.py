582566fd3648e6e4bd02883c2b509c326f4fffd6
Anton Yefremov <anthyef@gmail.com>
2015-04-15 01:32:37 +0300

    settings, requirements and gitignore

--- tsppgallery/settings.py
+++ tsppgallery/settings.py
@@ -108,3 +108,20 @@ TEMPLATE_DIRS = (
 
 LOGIN_REDIRECT_URL = 'mainpage'
 LOGIN_URL = 'login'
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+
+#DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
