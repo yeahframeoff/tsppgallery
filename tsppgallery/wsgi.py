56f86a36d4e2304afab2c90ee07bdfdfe78774ea
Anton Yefremov <anthyef@gmail.com>
2015-04-15 02:11:54 +0300

    removed whitenoise usage, gonna see what happens

--- tsppgallery/wsgi.py
+++ tsppgallery/wsgi.py
@@ -15,6 +15,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tsppgallery.settings")
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
