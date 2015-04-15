bb0760a9486e93bce76ecc7efc6dd48d60694770
Anton Yefremov <anthyef@gmail.com>
2015-04-15 16:48:21 +0300

    markup

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = (
     url(r'^$', views.main, name='mainpage'),
-    url(r'^login/?$', authviews.login, {'template_name': 'templates/login.html', 'current_app': 'gallery'}, name='login'),
+    url(r'^login/?$', authviews.login, {'template_name': 'login.html', 'current_app': 'gallery'}, name='login'),
     url(r'^logout/?$', authviews.logout, {'next_page': 'mainpage', 'current_app': 'gallery'}, name='logout'),
     url(r'^register/?$', views.register, name='register'),
 )
