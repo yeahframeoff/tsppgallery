c9df464e4da0fe7c50a91c6d1522858185c0688d
Anton Yefremov <anthyef@gmail.com>
2015-05-02 21:04:08 +0300

    User proxies

--- gallery/models.py
+++ gallery/models.py
@@ -1,1 +1,24 @@
-from .gauth import User
+from .gauth import Role, User, UserManager
+from django.db import models
+
+def make_user_proxy(role):
+    if role not in Role.ROLES_LIST:
+        raise ValueError('role must be the one from Role.ROLES_LIST')
+    class ProxyUserManager(UserManager):
+        def get_queryset(self):
+            return super(ProxyUserManager, self)\
+                .get_queryset().filter(role=role)
+
+    class TheProxy(User):
+        objects = ProxyUserManager()
+        class Meta(User.Meta):
+            proxy = True
+        def save(self, *args, **kwargs):
+            self.role = role
+            super(Admin, self).save(*args, **kwargs)
+    return TheProxy
+
+
+Admin = make_user_proxy(Role.ADMIN)
+Artist = make_user_proxy(Role.ARTIST)
+Organizer = make_user_proxy(Role.ORGANIZER)
